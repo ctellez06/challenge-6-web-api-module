fec2703c350715a1eb0cf2c47d7ddec9a1d281bb
Chris T <ctellez06@gmail.com>
2021-08-25 13:45:54 -0700

    completed challenge code

---
+++ Develop/js/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+
+    {
+        title: "Is JavaScipt free: ",
+        choices: ["Yes", "No"],
+        answer: "Yes"
+    },
+
+    {
+        title: "Where can I download JavaScript: ",
+        choices: ["On Computer", "On Tablet", "On Smart-phone", "All of the above"],
+        answer: "All of the above"
+    },
+
+    {
+        title: "Does JavaScript accept double or single quotes: ",
+        choices: ["both", "double", "single", "none"],
+        answer: "both"
+    },
+
+    {
+        title: "JavaScript syntax defines which values: ",
+        choices: ["fixed values", "variable values", "no values", "both fixed and variable values"],
+        answer: "both fixed and variable values"
+    },
+
+    {
+        title: "Can Strings be written in double or single quotes: ",
+        choices: ["double", "single", "neither", "both double and single"],
+        answer: "both double and single"
+    }
+
+];
